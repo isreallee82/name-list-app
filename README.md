7f9d514223e1fa5ef815ccbdb353b142681d66c3
israellee <isreallee82@gmail.com>
2023-03-11 00:26:21 +0100

    name-list-app

--- README.md
+++ README.md
@@ -1,70 +1,33 @@
-# Getting Started with Create React App
+## Name List App
+A simple React application that allows the user to add, edit, and delete names. The names are stored in local storage and persist between sessions.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Technologies
+React
+HTML
+CSS
+JavaScript
+Features
+Add names to the list
+Edit existing names
+Delete names from the list
+Names are stored in local storage and persist between sessions
 
-## Available Scripts
+## Installation
+Clone the repository: git clone https://github.com/isreallee82/name-list-app.git
 
-In the project directory, you can run:
+make sure you have node installed and npm
+-- Navigate to the project directory: cd name-list-app
+-- Install dependencies: npm install
+-- Start the app: npm start
+-- Open the app in your browser at http://localhost:3000
 
-### `npm start`
+## Usage
+Enter a comma-separated list of names in the input field and click "Add Names"
+To edit a name, click the yellow edit button and enter the new name in the prompt that appears
+To delete a name, click the red delete button
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Contributing
+Contributions are welcome! Please create an issue to discuss any changes you would like to make before submitting a pull request.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## License
+This project is licensed under the MIT License. See the LICENSE file for more information.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [names, setNames] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  useEffect(() => {
+    const storedNames = localStorage.getItem('names')
+    if (storedNames) {
+      setNames(JSON.parse(storedNames))
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('names', JSON.stringify(names))
+  }, [names])
+
+  const handleAddNames = (event) => {
+    event.preventDefault()
+    const newNames = inputValue.split(',')
+    const filteredNames = newNames.filter((name) => name.trim() !== '') // filter out empty names
+    if (filteredNames.length > 0) {
+      setNames((prevNames) => [...prevNames, ...filteredNames])
+      setInputValue('')
+    }
+  }
+
+  const handleDeleteName = (index) => {
+    setNames((prevNames) => {
+      const newNames = [...prevNames]
+      newNames.splice(index, 1)
+      return newNames
+    })
+  }
+
+  const handleEditName = (index, newName) => {
+    setNames((prevNames) => {
+      const newNames = [...prevNames]
+      newNames[index] = newName
+      return newNames
+    })
+  }
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col items-center justify-center container mx-auto text-base'>
+      <form onSubmit={handleAddNames} className='my-4'>
+        <div className='text-sm'>
+          <label className='block mb-2'>
+            Enter names:
+            <input
+              type='text'
+              name='names'
+              className='border border-gray-400 rounded ml-2'
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <div className='flex justify-center'>
+          <button
+            type='submit'
+            className='rounded-full px-2 py-1 text-xs bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300'
+          >
+            Add Names
+          </button>
+        </div>
+      </form>
+      <div className='my-4'>
+        {names.map((name, index) => (
+          <div
+            key={index}
+            className='ring-1 bg-gray-100 bg-blur pb-2 inline-block  rounded-full items-center px-2  mr-2 py-1 mb-2'
+          >
+            <span className='mr-2 text-xs'>{name}</span>
+            <button
+              onClick={() => handleDeleteName(index)}
+              className='bg-red-500 text-white px-2 py-1 rounded'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                class='w-2 h-2'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z'
+                />
+              </svg>
+            </button>
+            <button
+              onClick={() => {
+                const newName = window.prompt('Enter new name:', name)
+                if (newName) {
+                  handleEditName(index, newName)
+                }
+              }}
+              className='bg-yellow-500 text-white px-1 py-1 rounded ml-2'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-2 h-2'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
+                />
+              </svg>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
